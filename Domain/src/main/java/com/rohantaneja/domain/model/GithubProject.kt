9ab11a684e27613ec17c47e237d4379ec9ad4243
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-06-26 22:01:45 +0530

    added model class for Github trending project

--- Domain/src/main/java/com/rohantaneja/domain/model/GithubProject.kt
+++ Domain/src/main/java/com/rohantaneja/domain/model/GithubProject.kt
@@ -1,2 +1,5 @@
 package com.rohantaneja.domain.model
 
+class GithubProject(val id: String, val name: String, val fullName: String, val starCount: String,
+                    val dateCreated: String, val ownerName: String, val ownerAvatar: String,
+                    val isBookmarked: Boolean)
